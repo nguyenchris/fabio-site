4b2e284e564dc7cdc5686e594b995f5f97724bfb
Chris Nguyen <34232894+nguyenchris@users.noreply.github.com>
2019-06-26 04:45:05 -0700

    finished layout and beginning landing page

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,5 +2,9 @@ $(document).ready(function() {
   $('.js-scroll-meals').click(function(e) {
     $('html, body').animate({ scrollTop: $('.section-meals').offset().top }, 1000);
   });
+
+  $('.carousel').carousel({
+    interval: 5500
+  })
 });
 
